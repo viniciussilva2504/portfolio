8687f595f681c105c86cae21a65f78a857ff519f
Vinic <vinic@gamestore.com>
2026-04-09 21:23:20 +0100

    repara componentes index e tsconfig.json

--- src/components/Projeto/index.tsx
+++ src/components/Projeto/index.tsx
@@ -2,190 +2,20 @@ import { Card, LinkBotao } from './styles'
 import Title from '../title'
 import Paragrafo from '../Paragrafo'
 
-export const ProjetoDisneyPlus = () => {
-  return (
-    <Card>
-      <Title>Projeto Clone Disney+</Title>
-      <Paragrafo tipo="secundario">
-        Front-end clone da interface do Disney+ usando React
-      </Paragrafo>
-      <LinkBotao
-        href="https://clone-disneyplus-mu-two.vercel.app/"
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-        Visualizar
-      </LinkBotao>
-    </Card>
-  )
+export type ProjetoProps = {
+  titulo: string
+  descricao: string
+  link: string
 }
-export const EbacTechTalks = () => {
-  return (
-    <Card>
-      <Title>Evento Fictício</Title>
-      <Paragrafo tipo="secundario">
-        EBAC Tech Talks - Divulgação de evento fictício
-      </Paragrafo>
-      <LinkBotao
-        href="https://ebac-tech-talks-azure-seven.vercel.app/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Visualizar
-      </LinkBotao>
-    </Card>
-  )
-}
-export const Efood = () => {
-  return (
-    <Card>
-      <Title>Projeto eFood API</Title>
-      <Paragrafo tipo="secundario">
-        API RESTful para um aplicativo de delivery de comida
-      </Paragrafo>
-      <LinkBotao
-        href="https://efood-api-mu.vercel.app/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Visualizar
-      </LinkBotao>
-    </Card>
-  )
-}
-export const ListaDeTarefas = () => {
-  return (
-    <Card>
-      <Title>Lista de Tarefas</Title>
-      <Paragrafo tipo="secundario">
-        Gerenciador de tarefas desenvolvido com React
-      </Paragrafo>
-      <LinkBotao
-        href="https://minhas-tarefas-qbvq6g7hg-vinicius-silvas-projects-6f23ba6d.vercel.app/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Visualizar
-      </LinkBotao>
-    </Card>
-  )
-}
-export const ListaDeContatos = () => {
-  return (
-    <Card>
-      <Title>Lista de Contatos</Title>
-      <Paragrafo tipo="secundario">
-        Gerenciador de contatos desenvolvido com React
-      </Paragrafo>
-      <LinkBotao
-        href="https://lista-de-contatos-react-iota-navy.vercel.app/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Visualizar
-      </LinkBotao>
-    </Card>
-  )
-}
-export const CalculadoraAritmetica = () => {
-  return (
-    <Card>
-      <Title>Calculadora Aritmética Vue.js</Title>
-      <Paragrafo tipo="secundario">
-        Calculadora Aritmética desenvolvida com Vue.js
-      </Paragrafo>
-      <LinkBotao
-        href="https://calculadora-vuejs-theta.vercel.app/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Visualizar
-      </LinkBotao>
-    </Card>
-  )
-}
-export const CalculadoraImcReact = () => {
-  return (
-    <Card>
-      <Title>Calculadora IMC React</Title>
-      <Paragrafo tipo="secundario">
-        Calculadora de IMC desenvolvida com React
-      </Paragrafo>
-      <LinkBotao
-        href="https://calc-imc-reactjs-jade.vercel.app/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Visualizar
-      </LinkBotao>
-    </Card>
-  )
-}
-export const CalculadoraMediasEscolares = () => {
-  return (
-    <Card>
-      <Title>Calculadora de Médias Escolares</Title>
-      <Paragrafo tipo="secundario">
-        Calculadora de médias escolares em React
-      </Paragrafo>
-      <LinkBotao
-        href="https://calculadora-medias-proj.vercel.app/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Visualizar
-      </LinkBotao>
-    </Card>
-  )
-}
-export const MktGaleria = () => {
-  return (
-    <Card>
-      <Title>Galeria de Arte - Proj. HTML</Title>
-      <Paragrafo tipo="secundario">
-        Projeto HTML/CSS de uma galeria de arte fictícia
-      </Paragrafo>
-      <LinkBotao
-        href="https://email-mkt-galeria.vercel.app/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Visualizar
-      </LinkBotao>
-    </Card>
-  )
-}
-export const TesteCypress = () => {
-  return (
-    <Card>
-      <Title>Teste Automatizado com Cypress</Title>
-      <Paragrafo tipo="secundario">
-        Teste automatizado de API web em Cypress
-      </Paragrafo>
-      <LinkBotao
-        href="https://github.com/viniciussilva2504/exercicio-cypress.git"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Visualizar
-      </LinkBotao>
-    </Card>
-  )
-}
-export const ProdDadosPython = () => {
-  return (
-    <Card>
-      <Title>Dados com Python</Title>
-      <Paragrafo tipo="secundario">
-        Produção de dados gráficos e análises utilizando Python
-      </Paragrafo>
-      <LinkBotao
-        href="https://github.com/viniciussilva2504/extracao.py.git"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Visualizar
-      </LinkBotao>
-    </Card>
-  )
-}
+
+const Projeto = ({ titulo, descricao, link }: ProjetoProps) => (
+  <Card>
+    <Title>{titulo}</Title>
+    <Paragrafo tipo="secundario">{descricao}</Paragrafo>
+    <LinkBotao href={link} target="_blank" rel="noopener noreferrer">
+      Visualizar
+    </LinkBotao>
+  </Card>
+)
+
+export default Projeto
--- src/containers/Projetos/index.tsx
+++ src/containers/Projetos/index.tsx
@@ -1,58 +1,81 @@
-import {
-  ProjetoDisneyPlus,
-  EbacTechTalks,
-  Efood,
-  ListaDeTarefas,
-  ListaDeContatos,
-  CalculadoraAritmetica,
-  CalculadoraImcReact,
-  CalculadoraMediasEscolares,
-  MktGaleria,
-  TesteCypress,
-  ProdDadosPython,
-} from '../../components/Projeto'
+import Projeto from '../../components/Projeto'
+import type { ProjetoProps } from '../../components/Projeto'
 import Title from '../../components/title'
 
 import { Lista } from './styles'
+
+const projetos: ProjetoProps[] = [
+  {
+    titulo: 'Projeto eFood API',
+    descricao: 'API RESTful para um aplicativo de delivery de comida',
+    link: 'https://efood-api-mu.vercel.app/',
+  },
+  {
+    titulo: 'Projeto Clone Disney+',
+    descricao: 'Front-end clone da interface do Disney+ usando React',
+    link: 'https://clone-disneyplus-mu-two.vercel.app/',
+  },
+  {
+    titulo: 'Evento Fictício',
+    descricao: 'EBAC Tech Talks - Divulgação de evento fictício',
+    link: 'https://ebac-tech-talks-azure-seven.vercel.app/',
+  },
+  {
+    titulo: 'Lista de Tarefas',
+    descricao: 'Gerenciador de tarefas desenvolvido com React',
+    link: 'https://minhas-tarefas-qbvq6g7hg-vinicius-silvas-projects-6f23ba6d.vercel.app/',
+  },
+  {
+    titulo: 'Lista de Contatos',
+    descricao: 'Gerenciador de contatos desenvolvido com React',
+    link: 'https://lista-de-contatos-react-iota-navy.vercel.app/',
+  },
+  {
+    titulo: 'Calculadora Aritmética Vue.js',
+    descricao: 'Calculadora Aritmética desenvolvida com Vue.js',
+    link: 'https://calculadora-vuejs-theta.vercel.app/',
+  },
+  {
+    titulo: 'Calculadora IMC React',
+    descricao: 'Calculadora de IMC desenvolvida com React',
+    link: 'https://calc-imc-reactjs-jade.vercel.app/',
+  },
+  {
+    titulo: 'Galeria de Arte - Proj. HTML',
+    descricao: 'Projeto HTML/CSS de uma galeria de arte fictícia',
+    link: 'https://email-mkt-galeria.vercel.app/',
+  },
+  {
+    titulo: 'Calculadora de Médias Escolares',
+    descricao: 'Calculadora de médias escolares em React',
+    link: 'https://calculadora-medias-proj.vercel.app/',
+  },
+  {
+    titulo: 'Teste Automatizado com Cypress',
+    descricao: 'Teste automatizado de API web em Cypress',
+    link: 'https://github.com/viniciussilva2504/exercicio-cypress.git',
+  },
+  {
+    titulo: 'Dados com Python',
+    descricao: 'Produção de dados gráficos e análises utilizando Python',
+    link: 'https://github.com/viniciussilva2504/extracao.py.git',
+  },
+]
 
 const Projetos = () => (
   <section>
     <Title fontSize={16}>Projects</Title>
     <br />
     <Lista>
-      <li>
-        <ProjetoDisneyPlus />
-      </li>
-      <li>
-        <EbacTechTalks />
-      </li>
-      <li>
-        <Efood />
-      </li>
-      <li>
-        <ListaDeTarefas />
-      </li>
-      <li>
-        <ListaDeContatos />
-      </li>
-      <li>
-        <CalculadoraAritmetica />
-      </li>
-      <li>
-        <CalculadoraImcReact />
-      </li>
-      <li>
-        <MktGaleria />
-      </li>
-      <li>
-        <CalculadoraMediasEscolares />
-      </li>
-      <li>
-        <TesteCypress />
-      </li>
-      <li>
-        <ProdDadosPython />
-      </li>
+      {projetos.map((projeto) => (
+        <li key={projeto.link}>
+          <Projeto
+            titulo={projeto.titulo}
+            descricao={projeto.descricao}
+            link={projeto.link}
+          />
+        </li>
+      ))}
     </Lista>
   </section>
 )
